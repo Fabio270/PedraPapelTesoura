3a0343c028c10bdb178148bda94eefc55b6811b3
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-05-07 20:39:05 -0300

    Feat: Now animation for both gamemodes are setted.

--- app/src/main/java/com/fabioseyiji/pedrapapeltesoura/MainActivity.kt
+++ app/src/main/java/com/fabioseyiji/pedrapapeltesoura/MainActivity.kt
@@ -9,13 +9,13 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
         amb.button1v1.setOnClickListener {
             val intent = Intent(this, PlayGame::class.java)
+            intent.putExtra("1v1", 1)
             startActivity(intent)
         }
 
--- app/src/main/java/com/fabioseyiji/pedrapapeltesoura/PlayGame.kt
+++ app/src/main/java/com/fabioseyiji/pedrapapeltesoura/PlayGame.kt
@@ -1,11 +1,57 @@
 package com.fabioseyiji.pedrapapeltesoura
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import com.fabioseyiji.pedrapapeltesoura.databinding.ActivityPlayGameBinding
 
 class PlayGame : AppCompatActivity() {
+
+    private val binding: ActivityPlayGameBinding by lazy {
+        ActivityPlayGameBinding.inflate(layoutInflater)
+    }
+
+    private var animation1: AnimationDrawable? = null
+    private var animation2: AnimationDrawable? = null
+    private var animation3: AnimationDrawable? = null
+    private var setTime: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play_game)
+        setContentView(binding.root)
+    }
+
+    private fun playAnimation() {
+        val gameMode = intent.getIntExtra("1v1", 0)
+
+        binding.apply {
+            youSelectionImage.setBackgroundResource(R.drawable.animation_rpc)
+            animation1 = youSelectionImage.background as AnimationDrawable
+            cpu1SelectionImage.setBackgroundResource(R.drawable.animation_rpc)
+            animation2 = cpu1SelectionImage.background as AnimationDrawable
+            if (gameMode == 2) {
+                cpu2Tv.visibility = View.VISIBLE
+                cpu2SelectionImage.visibility = View.VISIBLE
+                cpu2SelectionImage.setBackgroundResource(R.drawable.animation_rpc)
+                animation3 = cpu2SelectionImage.background as AnimationDrawable
+            }
+        }
+
+        setTime = object: CountDownTimer(3000, 1000){
+            override fun onTick(p0: Long) {
+                animation1?.start()
+                animation2?.start()
+                animation3?.start()
+            }
+
+            override fun onFinish() {
+                animation1?.stop()
+                animation2?.stop()
+                animation3?.stop()
+            }
+        }.start()
+
     }
 }
