60915de957b7c23a5aad6e29e77d332aa7ccc981
Fábio Seyiji <81870980+Fabio270@users.noreply.github.com>
2024-05-07 19:37:10 -0300

    Feat: New activity_play_game files created and created basic screen for it.

---
+++ app/src/main/java/com/fabioseyiji/pedrapapeltesoura/PlayGame.kt
@@ -0,0 +1,11 @@
+package com.fabioseyiji.pedrapapeltesoura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_game)
+    }
+}
